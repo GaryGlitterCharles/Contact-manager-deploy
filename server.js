f87a5da3c59e44334d852548fdd20978685f6303
Gary Glitter Charles <40486538+GaryGlitterCharles@users.noreply.github.com>
2019-05-12 14:19:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/Deploy-test'));
+app.use(express.static(__dirname + '/dist/ag-grid'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/Deploy-test/index.html'));
+res.sendFile(path.join(__dirname+'/dist/ag-grid/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
